2f301fefca36f0e53a7fe9c663d20acdea3f364e
Sajan <sajanmhatre29@gmail.com>
2021-11-09 16:18:00 +0530

    [Sajan]Add: Header and Footer Routing paths added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HeaderComponent } from './Components/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'header', component: HeaderComponent},
+  {path: 'footer', component: FooterComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
